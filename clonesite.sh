0d1b9ef3579154f18a2f74b80b3ed8bb0de07625
koltrast <koltrast@pm.me>
2020-04-30 16:13:09 +0200

    404 > not_found

--- clonesite.sh
+++ clonesite.sh
@@ -1,5 +1,6 @@
 git checkout build -- site &&
 git checkout build -- docs &&
+mv site/404.txt site/not_found.txt
 rm -f site/blog/mk-blog &&
 sed -i 's+koltrast.github.io+koltrast.neocities.org+g' site/blog/blog.xml &
 
