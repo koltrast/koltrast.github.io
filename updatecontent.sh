e426c79cc2d8074264d05d64ebc515251656fd67
koltrast <koltrast@pm.me>
2020-04-30 02:56:30 +0200

    updated content

--- updatecontent.sh
+++ updatecontent.sh
@@ -1,6 +1,7 @@
 
 
-git checkout build -- docs/ && 
-git mv docs/* . &&
+git checkout build -- docs/ &&
+rm -rf *.txt *.html blog
+git mv -f docs/* . &&
 rm -r docs && git add . &&
 git commit -m "updated content"
